e82118f5e5c7f5b97d4497ee06cebb66330fa2c8
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-09-14 21:56:25 +0200

    test 4 green

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,9 +1,14 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
 
     public static final double MAXIMUM_LOAD = 100.0d;
     private double currentLoadPercentage;
+    private List<Vm> vms = new ArrayList<Vm>();
     private int capacity;
 
     public Server(int capacity) {
@@ -19,6 +24,11 @@ public class Server {
     }
 
     public void addVm(Vm vm) {
+        vms.add(vm);
         currentLoadPercentage = (double) vm.getVmSize() / capacity * MAXIMUM_LOAD;
+    }
+
+    public int vmsCount() {
+        return vms.size();
     }
 }
---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentVmsCountMatcher.java
@@ -0,0 +1,26 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentVmsCountMatcher extends TypeSafeMatcher<Server> {
+
+    private int expectedAmount;
+
+    public CurrentVmsCountMatcher(int expectedAmount) {
+        this.expectedAmount = expectedAmount;
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description mismatchDescription) {
+        mismatchDescription.appendText("a server with vms count of").appendValue(item.vmsCount());
+    }
+
+    protected boolean matchesSafely(Server server) {
+        return server.vmsCount() == expectedAmount;
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("a server with vms count of ").appendValue(expectedAmount);
+    }
+}
