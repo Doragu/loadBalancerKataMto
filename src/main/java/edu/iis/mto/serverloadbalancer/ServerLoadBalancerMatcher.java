deeca2834ea0d4538b91cc0efdbc80e2ed6e1f26
Krzysztof Chybicki <215693@edu.p.lodz.pl>
2020-09-14 23:17:36 +0200

    test 1 green

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerMatcher.java
@@ -0,0 +1,21 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ServerLoadBalancerMatcher extends TypeSafeMatcher<Server> {
+
+    private double expectedValue;
+
+    public ServerLoadBalancerMatcher(double expectedValue) {
+        this.expectedValue = expectedValue;
+    }
+
+    protected boolean matchesSafely(Server server) {
+        return true;
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("a server with current load of").appendValue(expectedValue);
+    }
+}
